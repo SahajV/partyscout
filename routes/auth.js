64195e2b4f25d2bb87058ea008f17c0008aebe40
GildedBaboon <vedereysahaj1@gmail.com>
2020-03-28 14:03:25 -0400

    Added auth.js file and removed logout and login.js

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+module.exports.set = function (app) {
+
+    app.get('/login', (req, res) => {
+
+	});
+	
+	app.get('/logout', (req, res) => {
+
+	});
+	
+}
--- routes/logout.js
+++
@@ -1,7 +0,0 @@
-module.exports.set = function (app) {
-
-    app.get('/logout', (req, res) => {
-
-    });
-
-}
