e444fa0b8a794b3132bbb3bec43085cf796a8cc8
Sahaj V <sahaj@Surface.localdomain>
2020-03-28 13:55:17 -0400

    Added initial JS files

---
+++ routes/login.js
@@ -0,0 +1,7 @@
+module.exports.set = function (app) {
+
+    app.get('/login', (req, res) => {
+
+    });
+
+}
---
+++ routes/search.js
@@ -0,0 +1,7 @@
+module.exports.set = function (app) {
+
+    app.get('/search', (req, res) => {
+
+    });
+
+}
