857d3f14eb936e0b6f978438e4807d6b6c679b86
GildedBaboon <vedereysahaj1@gmail.com>
2020-03-28 15:32:16 -0400

    Fixed app.js error

--- routes/profile.js
+++ routes/profile.js
@@ -1,41 +1,48 @@
 module.exports.set = function (app) {
-    const { MongoClient } = require('mongodb');
+    // const MongoClient = require('mongodb').MongoClient;
 
-    app.get('/profile', (req, res) => {
+    // app.get('/profile', (req, res) => {
 
-    });
+    // });
 
-    async function listDatabases(client){
-        databasesList = await client.db().admin().listDatabases();
+    // async function listDatabases(client){
+    //     databasesList = await client.db().admin().listDatabases();
      
-        console.log("Databases:");
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-    };
+    //     console.log("Databases:");
+    //     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    // };
 
-    app.get('/get_profile', (req, res) => {
+    // async function main(){
 
-        const uri = "mongodb+srv://sahajV:<password>@gameconnectcluster-zoadx.gcp.mongodb.net/test?retryWrites=true&w=majority";
+    //     const uri = "mongodb+srv://sahajV:<BqBCuf7ID4vn7uEh>@gameconnectcluster-zoadx.gcp.mongodb.net/test?retryWrites=true&w=majority";
+    
+    //     const client = new MongoClient(uri);
+     
+    //     try {
+    //         // Connect to the MongoDB cluster
+    //         await client.connect();
+     
+    //         // Make the appropriate DB calls
+    //         await  listDatabases(client);
+     
+    //     } catch (e) {
+    //         console.error(e);
+    //     } finally {
+    //         await client.close();
+    //     }
+    // }
+    
+    // main().catch(console.error);
 
-        const client = new MongoClient(uri);
+    // app.get('/get_profile', (req, res) => {
 
-        try {
-            // Connect to the MongoDB cluster
-            await client.connect();
+    //     main();
 
-            // Make the appropriate DB calls
-            await listDatabases(client);
+    // });
 
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await client.close();
-        }
+    // app.post('/post_profile', (req, res) => {
 
-    });
-
-    app.post('/post_profile', (req, res) => {
-
-    });
+    // });
 
     app.post('/update_profile', (req, res) => {
 
