e8faf7573c96a98afc0f3508ad849e80e6fca3d5
Khavab <2021snomani@tjhsst.edu>
2020-03-28 20:05:00 -0400

    pages

--- routes/search.js
+++ routes/search.js
@@ -7,7 +7,7 @@ module.exports.set = function (app) {
     var gameDict = {0 : 'lol', 1 : 'cSGO', 2 : 'minecraft', 3 : 'r6', 4 : 'chat'};
     app.get('/gameFinder', (req, res) => {
         var game = parseFloat(req.query.game);
-        res.send("" + game);
+        res.render(gameDict[game]);
     });
 
 }
