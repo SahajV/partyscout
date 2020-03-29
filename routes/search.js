a51e8303bc417beb143426ff137d92c6332e0d46
John Pesavento <54077093+jpes707@users.noreply.github.com>
2020-03-29 11:53:42 -0400

    Fixed various errors

--- routes/search.js
+++ routes/search.js
@@ -7,9 +7,14 @@ module.exports.set = function (app) {
     });
 
     var gameDict = {0 : 'lol', 1 : 'cSGO', 2 : 'minecraft', 3 : 'r6', 4 : 'chat'};
-    app.get('/gameFinder', (req, res) => {
-        var game = parseFloat(req.query.game);
-        res.render(gameDict[game]);
+    app.get('/gameFinder', ensureAuthenticated, (req, res) => {
+        if (!('game' in req.query) || !('language' in req.query) || !(req.query.game in gameDict)) {
+            res.render('search');
+        }
+        else {
+            var game = parseFloat(req.query.game);
+            res.render(gameDict[game]);
+        }
     });
 
 }
