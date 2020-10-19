2a691fcdc5b487950ff610afac469dfd567305c5
John Pesavento <54077093+jpes707@users.noreply.github.com>
2020-10-19 11:25:48 -0400

    Updated authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    authURI: "mongodb+srv://sahajV:BqBCuf7ID4vn7uEh@gameconnectcluster-zoadx.gcp.mongodb.net/userLoginData?retryWrites=true&w=majority",
-    generalURI: "mongodb+srv://sahajV:BqBCuf7ID4vn7uEh@gameconnectcluster-zoadx.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    authURI: process.env.PARTYSCOUT_KEY.replace("<dbname>", "userLoginData"),
+    generalURI: process.env.PARTYSCOUT_KEY.replace("<dbname>", "test")
 };
